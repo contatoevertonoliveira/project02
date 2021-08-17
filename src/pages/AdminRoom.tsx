89f960e0d972b1032264704f0f913a63898ce1a9
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2021-08-16 21:44:55 -0300

    Ajustes Finais

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -6,6 +6,9 @@ import { useRoom } from '../hooks/useRoom';
 
 import logoImg from '../assets/logo.svg';
 import deleteImg from '../assets/delete.svg';
+import checkImg from '../assets/check.svg';
+import answerImg from '../assets/answer.svg';
+
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -42,6 +45,18 @@ export function AdminRoom(){
         }
     }
 
+    async function handleCheckQuestionAsAnswered(questionId: string){
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+        });
+    }
+
+    async function handleHighlightQuestion(questionId: string){
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighLighted: true,
+        });
+    }
+
     return(
         <div id="page-room">
             <header>
@@ -67,8 +82,27 @@ export function AdminRoom(){
                                 key={question.id} 
                                 content={question.content}
                                 author={question.author}
+                                isAnswered={question.isAnswered}
+                                isHighLighted={question.isHighLighted}
                             >
                                 
+                                {!question.isAnswered && (
+                                    <>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                                        >
+                                            <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleHighlightQuestion(question.id)}
+                                        >
+                                            <img src={answerImg} alt="Dar destaque a pergunta" />
+                                        </button>
+                                    </>
+                                )}
+
                                 <button
                                     type="button"
                                     onClick={() => handleDeleteQuestion(question.id)}
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -11,13 +11,6 @@ const firebaseConfig = {
     // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     // appId: process.env.REACT_APP_APP_ID
-      apiKey: "AIzaSyCtLkQTk7xA5CiELfFW9jzFGaKbVmQAC98",
-      authDomain: "letmeask-01-cc44f.firebaseapp.com",
-      databaseURL: "https://letmeask-01-cc44f-default-rtdb.firebaseio.com",
-      projectId: "letmeask-01-cc44f",
-      storageBucket: "letmeask-01-cc44f.appspot.com",
-      messagingSenderId: "728322300053",
-      appId: "1:728322300053:web:6357f498a4413a8d8de39c"
   };
 
   firebase.initializeApp(firebaseConfig);
